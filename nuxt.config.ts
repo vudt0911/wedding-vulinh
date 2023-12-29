23133283456771378c091a4ba707d9430ffbb438
The Vu <thevu091193@gmail.com>
2023-12-29 13:07:22 +0700

    update code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
 				{ property: 'twitter:title', content: process.env.APP_NAME },
 				{ property: 'twitter:image', content: `${process.env.APP_URL}/images/wish.jpg` },
 			],
-			title: process.env.APP_NAME,
+			title: process.env.APP_NAME || "NO title",
 		},
 	},
 	build: {
